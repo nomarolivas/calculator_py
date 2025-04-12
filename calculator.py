1273dce14aefa6252e1b68dfcd8092301cf97111
Nomar Olivas <nomarolivas@gmail.com>
2025-04-12 13:22:58 -0700

    Added Tkinter library

---
+++ calculator.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+root= tk.Tk()
+
+#label
+root.title("Calculator")
+root.geometry("400x500")
+root.config(bg="#282c35")  
+
+
+
+#User Entry
+
+
+
+
+
+# Runs TK 
+root.mainloop()
